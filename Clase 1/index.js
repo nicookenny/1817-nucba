9736a8dcd8870c4ff5fced697624fbc997a17a5f
Nicolas Kenny <nicolasdkenny@gmail.com>
2022-03-16 21:37:19 -0300

    first commit

---
+++ Clase 1/index.js
@@ -0,0 +1,23 @@
+const numero1 = 5;
+const numero2 = 5;
+
+// Operadores logicos
+/*
+AND = &&
+OR = ||
+NOT = !
+*/
+
+// Operadores de comparacion
+
+/*
+== => COMPARO EL VALOR (igual)
+!= => COMPARO EL VALOR (distinto)
+>  MAYOR QUE
+< MENOR QUE
+<= MENOR O IGUAL QUE
+>= MAYOR O IGUAL QUE
+=== => 
+!== => 
+*/
+Z
