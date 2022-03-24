ead6fa39659146817c0a7fe494fb066a51149d22
Nicolas Kenny <nicolasdkenny@gmail.com>
2022-03-23 23:19:55 -0300

    c 3

---
+++ Clase 3/index.js
@@ -0,0 +1,41 @@
+// Funciones
+function NOMBRE() {
+	// let r = 0
+	// if (1 > 2) {
+	// 	return 1;
+	// } else {
+	// 	return 2;
+	// }
+	// let r = 0;
+	// if (1 > 2) {
+	// 	r = 1;
+	// } else {
+	// 	r = 2;
+	// }
+	// return r;
+}
+// NOMBRE();
+// const valor = NOMBRE();
+
+// Ciclos
+
+// while (i < p) {
+// 	console.log(i);
+// 	i++;
+// }
+// let i = 0;
+// let p = 5;
+// do {
+// 	if (i === 3) break;
+// 	console.log(i);
+// 	i++;
+// } while (i < p);
+
+// const FRUTAS = [20, null, undefined, 'Hola', [], () => 'hola'];
+
+const nombre = 'Juan|Pedro|Matias|Fernando';
+console.log(nombre.split('|'));
+
+for (let i = 0; i < nombre.length; i++) {
+	console.log(nombre[i]);
+}
